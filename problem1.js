69b92b88fda62c490e2d76d348ae21358eb58800
Sarah DeBey <sarah.debey@gmail.com>
2016-04-15 13:44:56 -0700

    adds problem 3

--- problem1.js
+++ problem1.js
@@ -1,11 +1,23 @@
-// Write a function called multiply() that takes in two numbers as arguments and then returns the product of those numbers. Have it console.log() the numbers and the product with a message that EXACTLY follows this example and uses the values that were input into the function:
+// Write a function called sumAndMultiply() that takes in THREE numbers as arguments and then returns two values in an array: the sum of those numbers and the product of those numbers. Use your existing sum() and product() functions to do these calculations, and be sure to turn off their console.log()s by commenting them out. Have this function do a console.log() of the numbers, the sum, and the product with two separate messages that exactly follows the examples below and uses the values that were input into the function:
 //
-// "The product of 4 and 7 is 28."
+// "4 and 7 and 5 sum to 16."
+//
+// "The numbers 4 and 7 and 5 have a product of 140."
 
 function sum (x,y) {
-  console.log('"The sum of ' + x + ' and ' + y + ' is ' + (x + y) + '"');
+  //console.log('"The sum of ' + x + ' and ' + y + ' is ' + (x + y) + '"');
+  return x + y;
 }
 
 function multiply(x,y) {
-  console.log('"The product of ' + x + ' and ' + y + ' is ' + (x * y) + '"');
+  //console.log('"The product of ' + x + ' and ' + y + ' is ' + (x * y) + '"');
+  return x * y;
+}
+
+function sumAndMultiply(x,y,z) {
+  var sumProductArray = [];
+  sumProductArray.push(sum(x,y) + z);
+  sumProductArray.push(multiply(x,y) * z);
+  console.log('"' + x + ' and ' + y + ' and ' + z + ' sum to ' + sumProductArray[0] + '"');
+  console.log('"The numbers ' + x + ' and ' + y + ' and ' + z + ' have a product of ' + sumProductArray[1] + '"');
 }
