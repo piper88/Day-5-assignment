07b28b5fea44a12ad68202caef18edc242f58eb0
Sarah DeBey <sarah.debey@gmail.com>
2016-04-15 13:16:50 -0700

    adds problem-1

---
+++ problem1.js
@@ -0,0 +1,7 @@
+// (Don't forget to create a new branch!) Write a function called sum() that takes in two numbers as arguments and then returns the sum of those numbers. Have it console.log() the numbers and the sum with a message that EXACTLY follows this example and uses the values that were input into the function:
+//
+// "The sum of 4 and 7 is 11."
+
+function sum (x,y) {
+  console.log('"The sum of ' + x + ' and ' + y + ' is ' + (x + y) + '"');
+}
